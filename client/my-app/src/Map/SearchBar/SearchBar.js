beff2adc6b1b8f98dab539607cc51c936ecb175c
OwenWong0627 <owen.wong0627@gmail.com>
2021-08-21 13:39:58 -0400

    Add a base Searchbar

--- client/my-app/src/Map/SearchBar/SearchBar.js
+++ client/my-app/src/Map/SearchBar/SearchBar.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
-export default () => { 
-    return (
-        <div>
-            Search Bar
-        </div>
-    )
+function SearchBar() {
+   const [value, setValue] = useState("");
+   return (
+      <section className="Search">
+         <div className="container-fluid">
+            <div className="row justify-content-center">
+               <h1 className="Search__title">TITLE</h1>
+            </div>
+            <form>
+               <div className="row justify-content-center">
+                  <div className="col-4">
+                     <input
+                        size="large"
+                        className="Search__input"
+                        placeholder="Enter Your Location"
+                        value={value}
+                        onChange={(event) => {
+                           setValue(event.target.value);
+                        }}
+                     />
+                  </div>
+                  <div className="col-2">
+                     <button
+                        size="large"
+                        type="primary"
+                        htmlType="submit"
+                        className="Search__submit"
+                     >
+                        GO
+                     </button>
+                  </div>
+               </div>
+            </form>
+         </div>
+      </section>
+   )
 }
+
+export default SearchBar;
