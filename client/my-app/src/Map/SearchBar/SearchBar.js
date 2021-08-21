f799984a8c18eea5f0f9c1e54d9ee82a8e0f6bdf
OwenWong0627 <owen.wong0627@gmail.com>
2021-08-21 13:47:40 -0400

    Simplified Search Bar styling

--- client/my-app/src/Map/SearchBar/SearchBar.js
+++ client/my-app/src/Map/SearchBar/SearchBar.js
@@ -5,33 +5,29 @@ function SearchBar() {
    const [value, setValue] = useState("");
    return (
       <section className="Search">
-         <div className="container-fluid">
-            <div className="row justify-content-center">
+         <div className="Search__container">
+            <div className="Search__title__container">
                <h1 className="Search__title">TITLE</h1>
             </div>
             <form>
-               <div className="row justify-content-center">
-                  <div className="col-4">
-                     <input
-                        size="large"
-                        className="Search__input"
-                        placeholder="Enter Your Location"
-                        value={value}
-                        onChange={(event) => {
-                           setValue(event.target.value);
-                        }}
-                     />
-                  </div>
-                  <div className="col-2">
-                     <button
-                        size="large"
-                        type="primary"
-                        htmlType="submit"
-                        className="Search__submit"
-                     >
-                        GO
-                     </button>
-                  </div>
+               <div className="Search__bar">
+                  <input
+                     size="large"
+                     className="Search__input"
+                     placeholder="Enter Your Location"
+                     value={value}
+                     onChange={(event) => {
+                        setValue(event.target.value);
+                     }}
+                  />
+                  <button
+                     size="large"
+                     type="primary"
+                     htmlType="submit"
+                     className="Search__submit"
+                  >
+                     GO
+                  </button>
                </div>
             </form>
          </div>
