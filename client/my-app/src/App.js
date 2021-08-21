48364d0afe17bccd0158cd9a9cdca6f53f436861
Mit Patel <mitpatel2752@gmail.com>
2021-08-21 12:52:52 -0400

    Added data column

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Navbar from './Navbar/Navbar.js';
 import Home from './Home/Home.js';
+import DataDisplay from './DataDisplay/DataDisplay'
 
 function App() {
   return (
     <div>
         <Navbar/>
         <Home/>
+        <DataDisplay/>
     </div>
   );
 }
