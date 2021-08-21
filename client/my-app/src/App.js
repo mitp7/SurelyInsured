d6a001643aaa0f67c790b672bdf8f74804c0babc
Mit Patel <mitpatel2752@gmail.com>
2021-08-21 17:10:55 -0400

    Updated data board

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -9,13 +9,11 @@ function App() {
     <div>
         <Navbar/>
         <Home/>
-<<<<<<< HEAD
-=======
-        <DataDisplay/>
+
         <iframe src="https://compare.insurancehotline.com/en/auto/vehicles">
 
         </iframe>
->>>>>>> 3e02c2d0a011bdef6c92c3a999544e77bbeb54b7
+
     </div>
   );
 }
