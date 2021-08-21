5a438d2bc43e40b5c9d5e0a1d944ceb66bc6b77b
Aung Khant Min <aungkhantmin2014@gmail.com>
2021-08-21 18:44:07 -0400

    add landing page

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
 import Navbar from './Navbar/Navbar.js';
 import Home from './Home/Home.js';
+import Landing from './Landing/Landing.js';
 
 
 function App() {
   return (
-    <div>
+    <Router>
         <Navbar/>
-        <Home/>
-        
-    </div>
+        <Switch>
+          <Route path="/" exact render={ () => <Redirect to="/landing"/> }/>
+          <Route path="/landing" exact component={Landing}/>
+          <Route path="/app" exact component={Home} />
+        </Switch>
+    </Router>
   );
 }
 
