30ba0b49096f6cc24a25b43a200aa3d79adc32ee
OwenWong0627 <owen.wong0627@gmail.com>
2021-08-21 16:26:17 -0400

    Revert Change for now

--- client/my-app/src/Map/SearchBar/SearchBar.js
+++ client/my-app/src/Map/SearchBar/SearchBar.js
@@ -1,68 +1,38 @@
-import React from 'react';
-import usePlacesAutocomplete, {
-   getGeocode,
-   getLatLng,
-} from "use-places-autocomplete";
-import {
-   Combobox,
-   ComboboxInput,
-   ComboboxPopover,
-   ComboboxList,
-   ComboboxOption,
-} from "@reach/combobox";
+import React, { useState } from 'react';
 import './index.css';
 
-function SearchBar({ center, className }) {
-   const {
-      ready,
-      value,
-      suggestions: { status, data },
-      setValue,
-      clearSuggestions,
-   } = usePlacesAutocomplete({
-      requestOptions: {
-         location: { lat: () => center.lat, lng: () => center.lng },
-         radius: 200000,
-      }
-   });
+function SearchBar() {
+   const [value, setValue] = useState("");
    return (
-      <div className={className}>
-         <Combobox
-            onSelect={async (address) => {
-               setValue(address, false);
-               clearSuggestions();
-               try {
-                  const results = await getGeocode({ address });
-                  const { lat, lng } = await getLatLng(results[0]);
-                  // centerTo({ lat, lng });
-                  // save({ lat, lng, address });
-                  console.log(lat, lng);
-               }
-               catch {
-                  console.log("ERROR");
-               }
-            }}
-         >
-            <ComboboxInput
-               data-testid="search-bar"
-               value={value}
-               onChange={(event) => {
-                  setValue(event.target.value);
-               }}
-               disabled={!ready}
-               placeholder="Enter an address"
-            />
-            <ComboboxPopover>
-               <ComboboxList>
-                  {status === "OK" &&
-                     data.map(({ place_id, description }) => (
-                        <ComboboxOption key={place_id} value={description} />
-                     ))}
-               </ComboboxList>
-            </ComboboxPopover>
-         </Combobox>
-      </div>
-   );
+      <section className="Search">
+         <div className="Search__container">
+            <div className="Search__title__container">
+               <h1 className="Search__title">TITLE</h1>
+            </div>
+            <form>
+               <div className="Search__bar">
+                  <input
+                     size="large"
+                     className="Search__input"
+                     placeholder="Enter Your Location"
+                     value={value}
+                     onChange={(event) => {
+                        setValue(event.target.value);
+                     }}
+                  />
+                  <button
+                     size="large"
+                     type="primary"
+                     htmlType="submit"
+                     className="Search__submit"
+                  >
+                     GO
+                  </button>
+               </div>
+            </form>
+         </div>
+      </section>
+   )
 }
 
 export default SearchBar;
