91498d15522b608ab76721a7aa2d69870c7df17a
BornaSadeghi <sadeghiborna@gmail.com>
2021-08-21 18:53:09 -0400

    Add location method that returns severity score and mean number of incidents

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from data_analysis import get_location_data
 
 app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return 'Welcome to /'
+    return '<h1>The coolest insurance platform on the planet.</h1> This is where the server is hosted. Go to the client:'
+
+@app.route('/getdata', methods=['POST'])
+def get_data():
+    return get_location_data(45, -79)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='0.0.0.0')
