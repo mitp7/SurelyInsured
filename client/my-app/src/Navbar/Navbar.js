0a43871d0495938ad85e5e0d962b70243a5db310
Aung Khant Min <aungkhantmin2014@gmail.com>
2021-08-21 18:27:29 -0400

    convert to dark theme'

--- client/my-app/src/Navbar/Navbar.js
+++ client/my-app/src/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import './index.css'
 
 export default () => { 
     return (
-      <Navbar bg="light" expand="lg">
+      <Navbar className="navbar" expand="lg">
         <Container>
           <Navbar.Brand href="#home"><img className="logo" src={logo}/></Navbar.Brand>
           {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
