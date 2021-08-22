3104a95a625e919fa69915165b9457dc81459655
Mit Patel <mitpatel2752@gmail.com>
2021-08-21 22:57:41 -0400

    Aligned data box and title

--- client/my-app/src/DataDisplay/DataDisplay.js
+++ client/my-app/src/DataDisplay/DataDisplay.js
@@ -2,39 +2,41 @@ import "./DataDisplay.css";
 
 export default function DataDisplay() {
   return (
-    <div className="dataDisplay">
-      <h1>Your location risks result in insurance rates being high/low because:</h1>
-      <div className="featuredData">
-        <div className="dataInformation">
-          <span className="dataStatistics">32%</span>
-          <img src="/images/green_icon.png" alt=""></img>
-          <span className="dataCatergory">Lower rates of accidents</span>
-        </div>
-      </div>
+    <div className="heading">
+        <h1>Your location risks result in insurance rates being high/low because:</h1>
+        <div className="dataDisplay">
+            <div className="featuredData">
+                <div className="dataInformation">
+                <span className="dataStatistics">32%</span>
+                <img src="/images/green_icon.png" alt=""></img>
+                <span className="dataCatergory">Lower rates of accidents</span>
+                </div>
+            </div>
 
-      <div className="featuredData">
-        <div className="dataInformation">
-          <span className="dataStatistics">30%</span>
-          <img src="/images/red_icon.png" alt=""></img>
-          <span className="dataCatergory">Higher rates of accidents</span>
-        </div>
-      </div>
+            <div className="featuredData">
+                <div className="dataInformation">
+                <span className="dataStatistics">30%</span>
+                <img src="/images/red_icon.png" alt=""></img>
+                <span className="dataCatergory">Higher rates of accidents</span>
+                </div>
+            </div>
 
-      <div className="featuredData">
-        <div className="dataInformation">
-          <span className="dataStatistics">30%</span>
-          <img src="/images/green_icon.png" alt=""></img>
-          <span className="dataCatergory">Lower rate of accidents</span>
-        </div>
-      </div>
+            <div className="featuredData">
+                <div className="dataInformation">
+                <span className="dataStatistics">30%</span>
+                <img src="/images/green_icon.png" alt=""></img>
+                <span className="dataCatergory">Lower rate of accidents</span>
+                </div>
+            </div>
 
-      <div className="featuredData">
-        <div className="dataInformation">
-          <span className="dataStatistics">30%</span>
-          <img src="/images/red_icon.png" alt=""></img>
-          <span className="dataCatergory">Higher rates of accidents</span>
+            <div className="featuredData">
+                <div className="dataInformation">
+                <span className="dataStatistics">30%</span>
+                <img src="/images/red_icon.png" alt=""></img>
+                <span className="dataCatergory">Higher rates of accidents</span>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
